4a005396268f87efc23abc018df13e4003d75d9b
heyhuihui <72719741@qq.com>
2019-06-15 23:31:20 +0800

    temp

--- sql/cinema.sql
+++ sql/cinema.sql
@@ -277,6 +277,7 @@ CREATE TABLE `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `level` tinyint(4) DEFAULT 1,
+  `ticket_consumption` double DEFAULT 0,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id_uindex` (`id`),
   UNIQUE KEY `user_username_uindex` (`username`)
@@ -289,7 +290,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'testname','123456',1),(3,'test','123456',1),(5,'test1','123456',1),(7,'test121','123456',1),(8,'root','123456',3),(10,'roottt','123123',1),(12,'zhourui','123456',1),(13,'abc123','abc123',1),(15,'dd','123',1),(16,'AzureXH','441402026ok',1),(17,'manager','123456',4);
+INSERT INTO `user` VALUES (1,'testname','123456',1,0),(3,'test','123456',1,0),(5,'test1','123456',1,0),(7,'test121','123456',1,0),(8,'root','123456',2,0),(10,'roottt','123123',1,0),(12,'zhourui','123456',1,0),(13,'abc123','abc123',1,0),(15,'dd','123',1,0),(16,'AzureXH','441402026ok',1,0),(17,'manager','123456',3,0);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
